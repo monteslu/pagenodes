16b569f1f8fd830f5304c731ce392a788d542e72
samrocksc <samrocksc@gmail.com>
2015-12-10 23:45:54 -0700

    exec>module
    Not used, so commented out

--- src/editor/nodeDefs/js/index.js
+++ src/editor/nodeDefs/js/index.js
@@ -3,6 +3,7 @@ const nodeDefs = [
   //require('./core/catch'),
   //require('./core/status'),
   //require('./core/debug'),
+  //require('./core/exec'),
   require('./core/espeak'),
   require('./core/notify'),
   require('./core/inject'),

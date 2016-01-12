e19516d07c45cf2ccab9774542cde05781ee3d7a
Brett Warner <bkwarner22@gmail.com>
2016-01-12 14:07:13 -0700

    stylistic cleanup

--- nodes/core/core/60-espeak.js
+++ nodes/core/core/60-espeak.js
@@ -1,67 +1,51 @@
-/**
- * Copyright 2013 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
+'use strict';
 
 module.exports = function(RED) {
-    "use strict";
-    var util = require("util");
-    var events = require("events");
-    var debuglength = RED.settings.debugMaxLength||1000;
-    var useColors = false;
-    // util.inspect.styles.boolean = "red";
+  var util = require('util');
+  var events = require('events');
+  var debuglength = RED.settings.debugMaxLength||1000;
+  var useColors = false;
+  // util.inspect.styles.boolean = "red";
 
-    function EspeakNode(n) {
-        RED.nodes.createNode(this,n);
-        this.name = n.name;
-        this.variant = n.variant;
+  function EspeakNode(n) {
+    RED.nodes.createNode(this, n);
+    this.name = n.name;
+    this.variant = n.variant;
 
-        this.console = n.console;
-        this.active = (n.active === null || typeof n.active === "undefined") || n.active;
-        var node = this;
+    this.console = n.console;
+    this.active = (n.active === null || typeof n.active === 'undefined') || n.active;
+    var node = this;
 
-        this.on("input",function(msg) {
+    this.on('input',function(msg) {
 
-            if (this.active) {
-                sendEspeak({id:this.id,name:this.name,topic:msg.topic,msg:msg, variant: msg.variant || node.variant});
-            }
+      if (this.active) {
+        sendEspeak({id:this.id, name:this.name, topic:msg.topic, msg:msg, variant: msg.variant || node.variant});
+      }
 
-        });
-    }
-
-    RED.nodes.registerType("espeak",EspeakNode);
-
-    function sendEspeak(msg) {
-        RED.comms.publish("espeak",msg);
-    }
-
-
-    RED.events.on("rpc_espeak", function(data) {
-        var node = RED.nodes.getNode(data.params[0]);
-        var state = data.params[1];
-        if (node !== null && typeof node !== "undefined" ) {
-            if (state === "enable") {
-                node.active = true;
-                data.reply(200);
-            } else if (state === "disable") {
-                node.active = false;
-                data.reply(201);
-            } else {
-                data.reply(404);
-            }
-        } else {
-            data.reply(404);
-        }
     });
+  }
+
+  RED.nodes.registerType('espeak', EspeakNode);
+
+  function sendEspeak(msg) {
+    RED.comms.publish('espeak', msg);
+  }
+
+  RED.events.on('rpc_espeak', function(data) {
+    var node = RED.nodes.getNode(data.params[0]);
+    var state = data.params[1];
+    if (node !== null && typeof node !== 'undefined' ) {
+      if (state === 'enable') {
+        node.active = true;
+        data.reply(200);
+      } else if (state === 'disable') {
+        node.active = false;
+        data.reply(201);
+      } else {
+        data.reply(404);
+      }
+    } else {
+      data.reply(404);
+    }
+  });
 };
--- src/editor/nodeDefs/js/core/espeak.js
+++ src/editor/nodeDefs/js/core/espeak.js
@@ -2,96 +2,96 @@
 
 var _ = require('lodash');
 
-module.exports = function(RED){
-    var voices = [];
-    speechSynthesis.onvoiceschanged = function() {
-      voices = speechSynthesis.getVoices();
-    };
+module.exports = function(RED) {
+  var voices = [];
+  speechSynthesis.onvoiceschanged = function() {
+    voices = speechSynthesis.getVoices();
+  };
 
-    RED.nodes.registerType('espeak',{
-        category: 'output',
-        defaults: {
-            name: {value:''},
-            variant: {value:''},
-            active: {value:true}
-        },
-        label: function() {
-            return this.name || this.variant || 'espeak';
-        },
-        labelStyle: function() {
-            return this.name?'node_label_italic':'';
-        },
-        color:'#ffb6c1',
-        inputs:1,
-        outputs:0,
-        icon: 'debug.png',
-        align: 'right',
-        button: {
-            toggle: 'active',
-            onclick: function() {
+  RED.nodes.registerType('espeak',{
+    category: 'output',
+    defaults: {
+      name: {value: ''},
+      variant: {value: ''},
+      active: {value: true}
+    },
+    label: function() {
+      return this.name || this.variant || 'espeak';
+    },
+    labelStyle: function() {
+      return this.name ? 'node_label_italic' : '';
+    },
+    color:'#ffb6c1',
+    inputs:1,
+    outputs:0,
+    icon: 'debug.png',
+    align: 'right',
+    button: {
+      toggle: 'active',
+      onclick: function() {
 
-                var label = this.name||'espeak';
-                var node = this;
-                RED.comms.rpc('espeak', [this.id, (this.active?'enable':'disable')], function(result){
-                    if (result == 200) {
-                        RED.notify(node._('debug.notification.activated',{label:label}),'success');
-                    } else if (result == 201) {
-                        RED.notify(node._('debug.notification.deactivated',{label:label}),'success');
-                    }
-                });
-            }
-        },
-        oneditprepare: function() {
-          var selectedVariant = this.variant;
-          var dropdown = document.getElementById('node-input-variant');
+        var label = this.name||'espeak';
+        var node = this;
+        RED.comms.rpc('espeak', [this.id, (this.active ? 'enable' : 'disable')], function(result) {
+          if (result == 200) {
+            RED.notify(node._('debug.notification.activated', {label: label}), 'success');
+          } else if (result == 201) {
+            RED.notify(node._('debug.notification.deactivated', {label: label}), 'success');
+          }
+        });
+      }
+    },
+    oneditprepare: function() {
+      var selectedVariant = this.variant;
+      var dropdown = document.getElementById('node-input-variant');
 
-          voices.forEach(function(voice){
-            var newVoice = document.createElement('OPTION');
-            newVoice.text = voice.name;
-            newVoice.value = voice.voiceURI;
+      voices.forEach(function(voice){
+        var newVoice = document.createElement('OPTION');
+        newVoice.text = voice.name;
+        newVoice.value = voice.voiceURI;
 
-            if(voice.voiceURI === selectedVariant){
-              newVoice.selected = true;
-            };
+        if(voice.voiceURI === selectedVariant){
+          newVoice.selected = true;
+        };
 
-            dropdown.options.add(newVoice);
-          });
-        },
+        dropdown.options.add(newVoice);
+      });
+    },
 
-        onpaletteadd: function() {
+    onpaletteadd: function() {
 
-            this.handleDebugMessage = function(t,o) {
+      this.handleDebugMessage = function(t,o) {
 
-                if(typeof o.msg === 'string'){
-                    try{
-                        o.msg = JSON.parse(o.msg);
-                    }
-                    catch(err){
-                        console.log('error parsing notification', err);
-                    }
-                }
-
-                var msg = o.msg;
-                //do tts
-
-                console.log('espeak', msg);
-                var phrase = new SpeechSynthesisUtterance(String(msg.payload));
-                var voice = _.find(voices, { voiceURI: o.variant });
-
-                phrase.voice = voice;
-                phrase.pitch = parseInt(msg.pitch, 10) || 1;
-                phrase.speed = parseInt(msg.spped, 10) || 1;
-
-                speechSynthesis.speak(phrase);
-            };
-            RED.comms.subscribe('espeak',this.handleDebugMessage);
-
-
-        },
-        onpaletteremove: function() {
-            RED.comms.unsubscribe('espeak',this.handleDebugMessage);
-            // RED.sidebar.removeTab("debug");
-            // delete RED._debug;
+        if(typeof o.msg === 'string'){
+          try{
+            o.msg = JSON.parse(o.msg);
+          }
+          catch(err){
+            console.log('error parsing notification', err);
+          }
         }
-    });
+
+        var msg = o.msg;
+        //do tts
+
+        console.log('espeak', msg);
+        var phrase = new SpeechSynthesisUtterance(String(msg.payload));
+        var voice = _.find(voices, { voiceURI: o.variant });
+
+        phrase.voice = voice;
+        phrase.pitch = parseInt(msg.pitch, 10) || 1;
+        phrase.speed = parseInt(msg.spped, 10) || 1;
+
+        speechSynthesis.speak(phrase);
+      };
+      RED.comms.subscribe('espeak',this.handleDebugMessage);
+
+
+    },
+    onpaletteremove: function() {
+      RED.comms.unsubscribe('espeak',this.handleDebugMessage);
+      // RED.sidebar.removeTab("debug");
+      // delete RED._debug;
+    }
+  });
 };

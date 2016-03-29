3315ee448a2dd4dd1bd5fbce535e3f6ce66838b3
Sam C <samrocksc@gmail.com>
2016-03-29 17:34:15 -0400

    pushing up changes for more redundancy

--- src/red/nodes/nodeDefs/core/io/voicerec.js
+++ src/red/nodes/nodeDefs/core/io/voicerec.js
@@ -2,44 +2,45 @@ module.exports = function(RED) {
   function SpeechNode(config) {
     RED.nodes.createNode(this,config)
     var node = this;
-    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-    var recognition = new SpeechRecognition();
-    var msg = {};
-    recognition.lang = 'en-US';
-    recognition.interimResults = false;
-    recognition.maxAlternatives = 1;
+    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+    if(SpeechRecognition){
+      var recognition = new SpeechRecognition();
+      var msg = {};
+      recognition.lang = 'en-US';
+      recognition.interimResults = false;
+      recognition.maxAlternatives = 1;
 
-    if(recognition){
-      recognition.onerror = function(event) {
-        if(event.error === 'no-speech'){
-          console.log('No Speech Detected');
+      if(recognition){
+        recognition.onerror = function(event) {
+          if(event.error === 'no-speech'){
+            console.log('No Speech Detected');
+          }
         }
-      }
 
-      recognition.onend = function() {
-        if(node.isopen){
-          recognition.start();
+        recognition.onend = function() {
+          if(node.isopen){
+            recognition.start();
+          }
         }
+
+        recognition.onresult = function(event) {
+          msg.payload = event.results[0][0].transcript;
+          console.log('speech-recognition msg',msg);
+          node.send(msg);
+        }
+
+        recognition.start();
+        node.isopen = true;
+
+      } else{
+        console.log('Your browser does not have the capability of voice recognition');
       }
 
-      recognition.onresult = function(event) {
-        msg.payload = event.results[0][0].transcript;
-        console.log('speech-recognition msg',msg);
-        node.send(msg);
-      }
-
-      recognition.start();
-      node.isopen = true;
-
-    }, else{
-      console.log('Your browser does not have the capability of voice recognition');
+      node.on("close", () => {
+        node.isopen = false;
+        recognition.stop();
+      });
     }
-
-    node.on("close", () => {
-      node.isopen = false;
-      recognition.stop();
-    });
-
   }
   RED.nodes.registerType("voice rec",SpeechNode);
 }

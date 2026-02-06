9b56655847f5edafa3d6274939a08bf948c95430
Luis Montes <monteslu@gmail.com>
2026-02-06 10:32:11 -0700

    fix: add __APP_VERSION__ to ESLint globals
    The Vite define variable __APP_VERSION__ was not declared in ESLint globals,
    causing lint to fail on the WhatsNewDialog component.

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,10 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        __APP_VERSION__: 'readonly',
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
